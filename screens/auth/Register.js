9bc96e7e88658dbddeab22cab034775efc749884
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2024-07-06 15:04:20 -0300

    feat: SubmitButton Component

--- screens/auth/Register.js
+++ screens/auth/Register.js
@@ -1,11 +1,30 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { View, Text, StyleSheet, TextInput, Alert } from "react-native";
 import React, { useState } from "react";
 import InputBox from "../../components/Forms/InputBox";
+import SubmitButton from "../../components/Forms/SubmitButton";
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  function handleSubmit() {
+    try {
+      setLoading(true);
+      if (!name || !email || !password) {
+        Alert.alert("Please Fill All Fiels");
+        setLoading(false);
+        return;
+      }
+
+      setLoading(false);
+      console.log("Register Data==>", { name, email, password });
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Register</Text>
@@ -26,7 +45,17 @@ const Register = () => {
           setValue={setPassword}
         />
       </View>
-      <Text> {JSON.stringify({ name, email, password }, null, 4)} </Text>
+      {/* <Text> {JSON.stringify({ name, email, password }, null, 4)} </Text> */}
+
+      <SubmitButton
+        btnTitle="Register"
+        loading={loading}
+        handleSubmit={handleSubmit}
+      />
+
+      <Text style={styles.linkText} >
+        ALready Register Please <Text style={styles.link} >LOGIN</Text>
+      </Text>
     </View>
   );
 };
@@ -47,14 +76,12 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
 
-  inputBox: {
-    height: 40,
-    marginBottom: 20,
-    backgroundColor: "#ffffff",
-    borderRadius: 10,
-    marginTop: 10,
-    paddingLeft: 10,
-    color: "#af9f85",
+  linkText: {
+    textAlign: "center",
+  },
+
+  link: {
+    color: "red",
   },
 });
 
